855b4b76799e772c7dbb78d0e9e867642a27109a
Andrei Bondad <jabondad24@gmail.com>
2026-03-09 20:04:52 -0500

    Converted the javafx file to load in as fxml rather then javafx

--- src/main/java/JavaFX.java
+++ src/main/java/JavaFX.java
@@ -1,5 +1,7 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.Scene;
@@ -29,71 +31,88 @@ public class JavaFX extends Application {
 	//feel free to remove the starter code from this method
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("I'm a professional Weather App!");
-		ArrayList<Period> forecast = WeatherAPI.getForecast("LOT",77,70);
-		ArrayList<Period> points = MyWeatherAPI.getPointForecast("Chicago");
+//		primaryStage.setTitle("I'm a professional Weather App!");
+//		ArrayList<Period> forecast = WeatherAPI.getForecast("LOT",77,70);
+//		ArrayList<Period> points = MyWeatherAPI.getPointForecast("Chicago");
+//
+//		if (forecast == null){
+//			throw new RuntimeException("Forecast did not load");
+//		}
+//		//Scene 1 Labels
+//		temperature = new Label();
+//		weather = new Label();
+//		city = new Label();
+//		title = new Label();
+//		rainChance = new Label();
+//		loading = new Label();
+//		loading.setVisible(false);
+//
+//		//Scene 1 Label Set Text
+//		temperature.setText("Today's weather is: "+String.valueOf(points.get(0).temperature));
+//        weather.setText(points.get(0).shortForecast);
+//		city.setText(MyWeatherAPI.cityName);
+//		title.setText("Weather App");
+//		rainChance.setText("The chance of rain is " +String.valueOf(points.get(0).probabilityOfPrecipitation.value)+"%");
+//		loading.setText("Now Loading");
+//
+//		//Scene 1 Text Field
+//		searchField = new TextField();
+//		searchField.setPromptText("Search for a city...");
+//		searchField.setMinWidth(500);
+//		searchField.setOnAction(e -> {
+//			ArrayList<Period> newCity = MyWeatherAPI.getPointForecast(searchField.getText());
+//			if (newCity == null) {
+//				city.setText("City not found");
+//				temperature.setVisible(false);
+//				weather.setVisible(false);
+//				rainChance.setVisible(false);
+//			} else {
+//			city.setText(MyWeatherAPI.cityName);
+//			temperature.setVisible(true);
+//			weather.setVisible(true);
+//			rainChance.setVisible(true);
+//			temperature.setText("Today's weather is: " + String.valueOf(newCity.get(0).temperature));
+//			weather.setText(newCity.get(0).shortForecast);
+//			rainChance.setText("The chance of rain is " + String.valueOf(newCity.get(0).probabilityOfPrecipitation.value) + "%");
+//			}
+//		});
+//
+//		//Scene 1 Buttons
+//		homeBtn = new Button("Home");
+//		forecastBtn = new Button("Forecast");
+//
+//		//VBox, HBox, BorderPanes
+//		topBar = new HBox(20,title, searchField);
+//		topBar.setPadding(new Insets(10));
+//		sideBar = new VBox(5,homeBtn,forecastBtn);
+//		sideBar.setPadding(new Insets(10));
+//		infoBox = new VBox(20,city, temperature, weather, rainChance,loading);
+//		s1hb = new HBox(200,sideBar, infoBox);
+//		s1bp = new BorderPane();
+//		fullVbox = new VBox (20, topBar,s1hb);
+//		s1bp.setCenter(fullVbox);
+//
+//		//SetScene
+//		Scene scene = new Scene(s1bp, 700,700);
+//		primaryStage.setScene(scene);
+//		primaryStage.show();
 
-		if (forecast == null){
-			throw new RuntimeException("Forecast did not load");
+		try {
+			// Read file fxml and draw interface.
+			Parent root = FXMLLoader.load(getClass()
+					.getResource("/FXML/TodayForecast.fxml"));
+
+			primaryStage.setTitle("342 Prject 2 Weather App");
+			Scene s1 = new Scene(root, 500,500);
+
+
+			primaryStage.setScene(s1);
+			primaryStage.show();
+
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.exit(1);
 		}
-		//Scene 1 Labels
-		temperature = new Label();
-		weather = new Label();
-		city = new Label();
-		title = new Label();
-		rainChance = new Label();
-		loading = new Label();
-		loading.setVisible(false);
-
-		//Scene 1 Label Set Text
-		temperature.setText("Today's weather is: "+String.valueOf(points.get(0).temperature));
-        weather.setText(points.get(0).shortForecast);
-		city.setText(MyWeatherAPI.cityName);
-		title.setText("Weather App");
-		rainChance.setText("The chance of rain is " +String.valueOf(points.get(0).probabilityOfPrecipitation.value)+"%");
-		loading.setText("Now Loading");
-
-		//Scene 1 Text Field
-		searchField = new TextField();
-		searchField.setPromptText("Search for a city...");
-		searchField.setMinWidth(500);
-		searchField.setOnAction(e -> {
-			ArrayList<Period> newCity = MyWeatherAPI.getPointForecast(searchField.getText());
-			if (newCity == null) {
-				city.setText("City not found");
-				temperature.setVisible(false);
-				weather.setVisible(false);
-				rainChance.setVisible(false);
-			} else {
-			city.setText(MyWeatherAPI.cityName);
-			temperature.setVisible(true);
-			weather.setVisible(true);
-			rainChance.setVisible(true);
-			temperature.setText("Today's weather is: " + String.valueOf(newCity.get(0).temperature));
-			weather.setText(newCity.get(0).shortForecast);
-			rainChance.setText("The chance of rain is " + String.valueOf(newCity.get(0).probabilityOfPrecipitation.value) + "%");
-			}
-		});
-
-		//Scene 1 Buttons
-		homeBtn = new Button("Home");
-		forecastBtn = new Button("Forecast");
-
-		//VBox, HBox, BorderPanes
-		topBar = new HBox(20,title, searchField);
-		topBar.setPadding(new Insets(10));
-		sideBar = new VBox(5,homeBtn,forecastBtn);
-		sideBar.setPadding(new Insets(10));
-		infoBox = new VBox(20,city, temperature, weather, rainChance,loading);
-		s1hb = new HBox(200,sideBar, infoBox);
-		s1bp = new BorderPane();
-		fullVbox = new VBox (20, topBar,s1hb);
-		s1bp.setCenter(fullVbox);
-
-		//SetScene
-		Scene scene = new Scene(s1bp, 700,700);
-		primaryStage.setScene(scene);
-		primaryStage.show();
 	}
 
 }
