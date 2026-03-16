bf2140e152204b9a6e216efcfa8fe21400735e9f
Andrei Bondad <jabondad24@gmail.com>
2026-03-15 22:47:10 -0500

    Debug attempt (unsucessful)
    There's currently a problem for the daily temperature whenever it switches to a different city. The hours are not aligned with what the current local hours are for that city. Attempts currently unsucessful.

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -242,11 +242,12 @@ public class Controller implements  Initializable{
                 int dailyTempsIndex = 0;
 
                 for (int i = 0; i <= 15; i += 3) {
-                    if (pointsHourly.get(i).endTime.getHours() == 0) {
+                    System.out.println((dailyTimesIndex + 1) + ":" +pointsHourly.get(i).startTime.getHours());
+                    if (pointsHourly.get(i).startTime.getHours() == 0) {
                         dailyTimes[dailyTimesIndex] = "12AM";
-                    } else if (pointsHourly.get(i).endTime.getHours() == 12) {
+                    } else if (pointsHourly.get(i).startTime.getHours() == 12) {
                         dailyTimes[dailyTimesIndex] = "12PM";
-                    } else if (pointsHourly.get(i).endTime.getHours() > 12) {
+                    } else if (pointsHourly.get(i).startTime.getHours() > 12) {
                         dailyTimes[dailyTimesIndex] = pointsHourly.get(i).startTime.getHours() - 12 + "PM";
                     } else {
                         dailyTimes[dailyTimesIndex] = pointsHourly.get(i).startTime.getHours() + "AM";
